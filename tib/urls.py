8f136e1a20b6f491311504c276531a44a2ee9901
Ryan Stuart <ryan.stuart.85@gmail.com>
2012-12-09 00:54:31 +1000

    Added django-blog-zinnia which lead to big overhaul of the templates. Updated to Bootstrap 2.2.1.

--- tib/urls.py
+++ tib/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
@@ -14,18 +14,22 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 
     # Static views
-    (r'^$', TemplateView.as_view(template_name="index.html")),
-    (r'^about/$', TemplateView.as_view(template_name="about.html")),
-    (r'^contact/$', TemplateView.as_view(template_name="contact.html")),
-    (r'^create/$', TemplateView.as_view(template_name="create.html")),
-    (r'^faq/$', TemplateView.as_view(template_name="faq.html")),
-    (r'^fb_close/$', TemplateView.as_view(template_name="fb_close.html")),
+    url(r'^$', TemplateView.as_view(template_name="index.html"), name="home" ),
+    url(r'^about/$', TemplateView.as_view(template_name="about.html"), name="about"),
+    url(r'^contact/$', TemplateView.as_view(template_name="contact.html"), name="contact"),
+    url(r'^create/$', TemplateView.as_view(template_name="create.html"), name="create"),
+    url(r'^faq/$', TemplateView.as_view(template_name="faq.html"), name="faq"),
+    url(r'^fb_close/$', TemplateView.as_view(template_name="fb_close.html")),
 
     # Dynamic views
     url(r'^contact/send/$', 'tib.views.contact_email'),
     url(r'^result/$', 'tib.views.result'),
     url (r'^result/([\w=-]+)/?$', 'tib.views.status'),
+
+    # Blog
+    url(r'^blog/', include('zinnia.urls')),
+    url(r'^comments/', include('django.contrib.comments.urls')),
 )
